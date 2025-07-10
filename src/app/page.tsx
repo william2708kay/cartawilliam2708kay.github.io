fbf4e1a53a00f042eef5d10cbaffc64db944df38
mario rocha <mesacorta32@gmail.com>
2025-07-10 19:27:38 +0000

    Hola, Dais, ¿cómo estás? Estuve preparando esto en mis tiempos libres. O

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LetterOpener } from '@/components/letter-opener';
 
 const letterContent = `Mi querida Daiana,
 
-Hola, Dais, ¿cómo estás? Estuve preparando esto en mis tiempos libres. Originalmente era para tu cumple, pero supongo que hoy es el final... En fin, ya que trabajo en esto, qué mejor forma de transformar mi trabajo en algo especial para ti.
+Hola, Dais, ¿cómo estás? Estuve preparando esto en mis tiempos libres. Originalmente era para tu cumple, pero supongo que hoy es el final... En fin, ya que trabajo en esto, qué mejor forma de transformar mi trabajo en algo especial para ti te deje algo al final pero tendras que esperar.
 
 Gracias por acompañarme hoy a ver Superman. Es mi superhéroe favorito, junto con Spiderman, y aunque todavía no he visto la película a la hora que escribo esto, sé que estará genial. Me encanta el cine, pero lo más bonito de ir es tener tu compañía; la disfruto muchísimo.
 
