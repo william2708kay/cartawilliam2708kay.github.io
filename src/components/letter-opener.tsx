da71023ecea2138b35e565c5065a3b2de7ae3135
mario rocha <mesacorta32@gmail.com>
2025-07-10 17:04:04 +0000

    TODO ESTA DIRECTAMNETE EN PUBLIC ARREGLA LAS RUTAS

--- src/components/letter-opener.tsx
+++ src/components/letter-opener.tsx
@@ -139,7 +139,7 @@ export function LetterOpener({
           <RoseIcon className="absolute -bottom-16 -right-24 h-32 w-32 text-accent/30 opacity-20 rotate-45 animate-pulse-slow" />
           <div className="mb-8 w-[200px] h-[200px] flex items-center justify-center">
              <img 
-              src="/images_carta/principal.gif"
+              src="/principal.gif"
               alt="principal" 
               width={200} 
               height={200}
@@ -169,7 +169,7 @@ export function LetterOpener({
       <div className="fixed inset-0 bg-black flex items-center justify-center">
         <video
           ref={videoRef}
-          src="/images_carta/transicion.mp4"
+          src="/transicion.mp4"
           className="max-w-full max-h-full"
           onEnded={handleVideoEnd}
           muted
