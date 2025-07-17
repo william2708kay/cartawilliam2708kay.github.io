f7ee7864de5a7cb63d2aecb1515124ec29cb7fa7
mario rocha <mesacorta32@gmail.com>
2025-07-17 04:30:23 +0000

    CAMBIA LA LETRA A CARTA COMO ESTABA SE VEIA MAS ELEGANTE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,14..72,200..900;1,14..72,200..900&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased">
         {children}
